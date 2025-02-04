6f267dc071f991a051edc681c641f050ae125acf
WeeblyChar <kamishironata@gmail.com>
2025-02-04 16:10:44 +0700

    estate's sample model update for creating estate.property table within the database.

--- tutorials/estate/models/sample_model_1.py
+++ tutorials/estate/models/sample_model_1.py
@@ -1,4 +1,25 @@
-from odoo import models
+from odoo import models, fields
 
-class TestModel(models.Model):
-    _name = "test_model_estate"
+class SampleEstateModel(models.Model):
+    _name = "estate.property"
+    _description = "test model for estate, why are there so many variables? what?"
+    
+    
+    name = fields.Char('Estate Name', required=True)
+    description = fields.Text('Estate Description', required=True)
+    postcode = fields.Char('Postcode', required=True)
+    date_availability = fields.Date('date_availability', requred=True, default=fields.Date.today)
+    expected_price = fields.Float('expected_price',required=True)
+    selling_price = fields.Float('selling_price',required=True)
+    bedrooms = fields.Integer('bedrooms', required=True)
+    living_area = fields.Integer('living_area', default=0)
+    facades = fields.Integer('facades', default=0)
+    garage = fields.Boolean('garage', default=False)
+    garden = fields.Boolean('garden', default=False)
+    garden_area = fields.Integer('garden_area', default=0)
+    garden_orientation = fields.Selection(
+        string='Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help='Please select the garden\'s orientation relative to the property.'
+    )
+    
